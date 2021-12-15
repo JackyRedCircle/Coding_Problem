c10ef429cc278e113fd3fae80c0420f4ee148a4e
jackyRedCircle <jykim030119@gmail.com>
2021-12-15 21:43:43 +0900

    <fix> 기존 풀이 파일 전부 삭제 <add> BaekJoon [2884] 풀이 수정 (L18 수정)

--- src/BaekJoon/Main10718.java
+++
@@ -1,9 +0,0 @@
-package BaekJoon;
-
-public class Main10718 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 2; i++) {
-            System.out.println("강한친구 대한육군");
-        }
-    }
-}
