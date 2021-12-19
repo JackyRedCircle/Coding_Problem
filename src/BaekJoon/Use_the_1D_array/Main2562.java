f5f90c0745822b8e094f73760f4740c6677853e5
jackyRedCircle <jykim030119@gmail.com>
2021-12-20 05:45:15 +0900

    <fix> BaekJoon - [Use_the_1D_array]>{Main2562(오답 수정 후 성공)}

--- src/BaekJoon/Use_the_1D_array/Main2562.java
+++ src/BaekJoon/Use_the_1D_array/Main2562.java
@@ -6,30 +6,19 @@ public class Main2562 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] numArr = new int[9];
+        int max = 0;
+        int index = 0;
 
-        for (int i = 1; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             numArr[i] = s.nextInt();
-        }
-
-        int tmp = numArr[0];
-
-        for (int i = 1; i < 9; i++) {
-            if(numArr[i] > tmp) {
-                tmp = numArr[i];
+            if(numArr[i] > max) {
+                max = numArr[i];
+                index = i+1;
             }
             else {
                 continue;
             }
         }
-
-        for  (int i = 0; i < 9; i++) {
-            if (tmp == numArr[i]) {
-                System.out.println(tmp);
-                System.out.println(i);
-            }
-            else {
-                continue;
-            }
-        }
+        System.out.println(max+"\n"+index);
     }
 }
