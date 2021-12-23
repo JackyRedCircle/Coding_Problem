d0f3cfbb609e2435d30bf8c3a205c0229bc8a31f
jackyRedCircle <jykim030119@gmail.com>
2021-12-24 02:40:46 +0900

    <fix> BaekJoon - [2908] 1차도전: "틀렸습니다"

---
+++ src/BaekJoon/Main2908.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Main2908 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String in = s.nextLine();
+        String[] inArr = in.split(" ");
+        String[] n1Arr = inArr[0].split("");
+        String[] n2Arr = inArr[1].split("");
+        inArr[0] = "";
+        inArr[1] = "";
+        boolean tmp = false;
+        for (int i = 0; i < 3; i++) {
+            inArr[0] += n1Arr[2-i];
+            inArr[1] += n2Arr[2-i];
+
+            if (Integer.parseInt(n1Arr[2-i]) > Integer.parseInt(n2Arr[2-i])) {
+                tmp = true;
+            }
+        }
+        if (tmp) {
+            System.out.println(inArr[0]);
+        }
+        else {
+            System.out.println(inArr[1]);
+        }
+    }
+}
