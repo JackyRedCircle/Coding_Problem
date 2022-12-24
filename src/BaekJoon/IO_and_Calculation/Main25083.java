41b7883ffb2cb8c988229382ea5b21f5e22190f4
jackyRedCircle <jykim030119@gmail.com>
2022-12-24 18:53:33 +0900

    <add>

---
+++ src/BaekJoon/IO_and_Calculation/Main25083.java
@@ -0,0 +1,13 @@
+package BaekJoon.IO_and_Calculation;
+
+public class Main25083 {
+    public static void main(String[] args) {
+        System.out.print("""
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \\. ". L_r'
+                   `~\\/
+                      |
+                      |""");
+    }
+}
