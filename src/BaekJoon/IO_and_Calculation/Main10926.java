cf83f91fde8f66c7ba4e12ffc6a472d4ac75315c
jackyRedCircle <jykim030119@gmail.com>
2022-12-24 18:11:06 +0900

    <add> BaekJoon - [IO_and_Calculation]>{Main10926}

---
+++ src/BaekJoon/IO_and_Calculation/Main10926.java
@@ -0,0 +1,15 @@
+package BaekJoon.IO_and_Calculation;
+
+import java.util.Scanner;
+
+public class Main10926 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner((System.in));
+
+        String tmp = s.nextLine();
+
+        String symbol = "??!";
+
+        System.out.printf(tmp + symbol);
+    }
+}
