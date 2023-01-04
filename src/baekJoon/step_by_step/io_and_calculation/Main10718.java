f23969a5d5b8ff694693b3a2201c3f3181becd47
JackyRedCircle <jykim030119@gmail.com>
2023-01-04 20:48:48 +0900

    <fix>{readme} <delete>

--- src/baekJoon/step_by_step/io_and_calculation/Main10718.java
+++
@@ -1,9 +0,0 @@
-package baekJoon.step_by_step.io_and_calculation;
-
-public class Main10718 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 2; i++) {
-            System.out.println("강한친구 대한육군");
-        }
-    }
-}
--- src/baekJoon/step_by_step/io_and_calculation/Main2557.java
+++
@@ -1,7 +0,0 @@
-package baekJoon.step_by_step.io_and_calculation;
-
-public class Main2557 {
-    public static void main(String[] args) {
-        System.out.printf("Hello World!");
-    }
-}
